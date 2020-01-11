e86f37803abd6b58f6c51d03a2eaae490642d983
haphamo <hapham0392@gmail.com>
2020-01-11 18:19:10 -0500

    Separated css styling from component

---
+++ src/components/OurValues/OurValues.css.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const CommunityValuesSection = styled.div`;
+background: #F5F5F5;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding-top: 60px;`
+
+const Headline1 = styled.header`
+font-family: 'Rubik', sans-serif;
+color: #255957;
+font-size: 56px;
+font-weight: bold;
+`
+const Headline2 = styled.header`
+font-family: 'Rubik', sans-serif;
+font-weight: medium;
+font-size: 28px;
+color: #333333
+padding-top: 20px;
+`
+const ValueSection = styled.div`
+display: flex;
+flex-direction: row;
+padding-top: 60px;
+`
+
+const Value = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+letter-spacing: -0.015em;
+`
+const Body = styled.div`
+font-family: 'Karla', sans-serif;
+font-size: 16px;
+font-weight: regular;
+line-height: 30px;
+color: #333333
+text-align: center;
+padding: 19px 20px 86px 20px;`
+
+export { CommunityValuesSection, Headline1, Headline2, ValueSection, Value, Body };
--- src/components/OurValues/OurValues.js
+++ src/components/OurValues/OurValues.js
@@ -1,49 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+import { 
+  CommunityValuesSection,
+  Headline1,
+  Headline2,
+  ValueSection,
+  Value, 
+  Body } from './OurValues.css'
 import inclusivityImg from '../../../src/images/22-web_essential-512.png'
 import communityImg from '../../../src/images/iconfinder_05-web_essential_3401825.svg'
 import developmentImg from '../../../src/images/iconfinder_27-web_essential_3401845.svg'
-
-const CommunityValuesSection = styled.div`;
-background: #F5F5F5;
-display: flex;
-flex-direction: column;
-align-items: center;
-padding-top: 60px;`
-
-const Headline1 = styled.header`
-font-family: 'Rubik', sans-serif;
-color: #255957;
-font-size: 56px;
-font-weight: bold;
-`
-const Headline2 = styled.header`
-font-family: 'Rubik', sans-serif;
-font-weight: medium;
-font-size: 28px;
-color: #333333
-padding-top: 20px;
-`
-const ValueSection = styled.div`
-display: flex;
-flex-direction: row;
-padding-top: 60px;
-`
-
-const Value = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-letter-spacing: -0.015em;
-`
-const Body = styled.div`
-font-family: 'Karla', sans-serif;
-font-size: 16px;
-font-weight: regular;
-line-height: 30px;
-color: #333333
-text-align: center;
-padding: 19px 20px 86px 20px;`
 
 export default function OurValues() {
   return (
