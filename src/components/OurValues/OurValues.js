54ed6b6072fed8030cd0f003c2b2390ac34e6a85
haphamo <hapham0392@gmail.com>
2020-01-11 16:22:13 -0500

    created styling components based off Figma wireFrame

--- src/components/OurValues/OurValues.js
+++ src/components/OurValues/OurValues.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Headline1 = styled.header`
+font-family: 'Rubik', sans-serif;
+color: #255957;
+font-size: 56px;
+font-weight: bold;
+`
+
+const CommunityValuesSection = styled.section`;
+background: #F5F5F5;`
+
 
 export default function OurValues() {
   return (
-    <header>Our Values</header>
+    
+    <CommunityValuesSection>
+      <Headline1>Our Values</Headline1>
+      {/* <h4>An Inclusive Toronto-based Tech Community</h4> */}
+
+    </CommunityValuesSection>
+    
   )
 }
