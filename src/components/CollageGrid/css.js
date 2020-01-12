9b45a01c51da318e1f02dca01fc03e51729a44e0
Jonathan Wong <jonathan.wong@applydigital.co>
2020-01-11 19:13:30 -0500

    Started grid

---
+++ src/components/CollageGrid/css.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-row: repeat(10, 5vw);
+`
+
+export const UL = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
+
+export const PicOne = styled.li``
+export const PicTwo = styled.li``
+export const PicThree = styled.li``
+export const PicFour = styled.li``
+export const PicFive = styled.li``
+export const PicSix = styled.li``
+export const PicSeven = styled.li``
+export const PicEight = styled.li``
---
+++ src/components/CollageGrid/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import {
+  Grid,
+  PicOne,
+  PicTwo,
+  PicThree,
+  PicFour,
+  PicFive,
+  PicSix,
+  PicSeven,
+} from "./css"
+function CollageGrid() {
+  return (
+    <Grid>
+      <PicOne />
+      <PicTwo />
+      <PicThree />
+      <PicFour />
+      <PicFive />
+      <PicSix />
+      <PicSeven />
+    </Grid>
+  )
+}
+
+export default CollageGrid
