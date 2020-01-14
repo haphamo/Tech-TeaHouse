af6f8c809539da4a87708bd557cc90f2100f392b
haphamo <hapham0392@gmail.com>
2020-01-14 18:18:31 -0500

    Separated data and renders successfully

--- src/components/OurValues/OurValues.js
+++ src/components/OurValues/OurValues.js
@@ -7,28 +7,14 @@ import {
   Value, 
   Body,
   IconImg } from './OurValues.css'
-import inclusivityImg from '../../../src/images/22-web_essential-512.png'
-import communityImg from '../../../src/images/iconfinder_05-web_essential_3401825.svg'
-import developmentImg from '../../../src/images/iconfinder_27-web_essential_3401845.svg'
 
-let fixtures = [
-  { src: inclusivityImg,
-    headline: "Inclusivity",
-    desc: "We are an inclusive tech community that cares for our member's professional development" },
-  { src: communityImg,
-    headline: "Community",
-    desc: "We are an inclusive tech community that cares for our member's professional development" },
-  { src: developmentImg,
-    headline: "Development",
-    desc: "We are an inclusive tech community that cares for our member's professional development" }
-]
-
+import { data } from './data'
 
 export default function OurValues() {
-  const allValues = fixtures.map(value => {
+  const allValues = data.map(value => {
     return(
       <Value key={value.headline}>
-        <IconImg classname={"icon"} src={value.src} alt={value.headline}/>
+        <IconImg classname={value.headline} src={value.src} alt={value.headline}/>
         <Headline2>{value.headline}</Headline2>
         <Body>{value.desc}</Body>
     </Value>
---
+++ src/components/OurValues/data.js
@@ -0,0 +1,17 @@
+import inclusivityImg from '../../../src/images/22-web_essential-512.png'
+import communityImg from '../../../src/images/iconfinder_05-web_essential_3401825.svg'
+import developmentImg from '../../../src/images/iconfinder_27-web_essential_3401845.svg'
+
+let data = [
+  { src: inclusivityImg,
+    headline: "Inclusivity",
+    desc: "We are an inclusive tech community that cares for our member's professional development" },
+  { src: communityImg,
+    headline: "Community",
+    desc: "We are an inclusive tech community that cares for our member's professional development" },
+  { src: developmentImg,
+    headline: "Development",
+    desc: "We are an inclusive tech community that cares for our member's professional development" }
+]
+
+export { data } 
