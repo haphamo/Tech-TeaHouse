fbccb879eb33d931b7619ad0fce5295875a91a30
haphamo <hapham0392@gmail.com>
2020-01-15 14:23:58 -0500

    Created TechCommunity component

---
+++ src/components/TechCommunity/TechCommunity.js
@@ -0,0 +1,14 @@
+import react from "react"
+
+export default function TechCommunity() {
+  return (
+    <div>
+      <h1>Building a More Inclusive Tech Community</h1>
+      <h6>Tech Teahouse is building a more diverse and inclusive tech community in Toronto by increasing access to tech-focused events, providing a forum to foster meaningful connections, and hosting free tech skills workshops for underrepresented groups. Some of our initiatives include:
+  
+      1) Tech Teahouse workshop series 2) Community forum  3) Regular member meetups 
+      4) Event notifications  5) Networking hub 6) Hackathon team building</h6>
+
+    </div>
+  )
+}
