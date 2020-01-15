22a01a5f1142c167eb03414f53a2198acea88b08
haphamo <hapham0392@gmail.com>
2020-01-15 17:24:39 -0500

    Pages are now functional, updated the links on the nav component

---
+++ src/pages/get-in-touch.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function GetInTouch() {
+  return (
+  <Layout pageName="get-in-touch">
+
+  </Layout>
+
+  )
+}
