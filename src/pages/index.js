b87afe70a6a398958c15d9fb9e0158f42366fdcc
Jonathan Wong <jonathan.wong@applydigital.co>
2020-01-16 18:02:47 -0500

    Added Gallery section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import Layout from "../components/layout"
 import OurValues from "../components/OurValues/OurValues"
 import Founders from "../components/Founders"
 import OurStory from "../components/OurStory"
-import CollageGrid from "../components/CollageGrid"
+import ImageGallery from "../components/ImageGallery"
 // Refer to src/Header/data for the types with corresponding object to pass to layout.
 const IndexPage = () => (
   <Layout pageName="landing-page">
     <OurValues />
     <Founders />
     <OurStory />
-    <CollageGrid />
+    <ImageGallery />
   </Layout>
 )
 
