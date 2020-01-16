20a7f09cef3f9ab4b827728e13d7cd44aeb6ed36
haphamo <hapham0392@gmail.com>
2020-01-15 19:33:40 -0500

    Changed unit sizes from px to rem units

--- src/components/OurValues/OurValues.css.js
+++ src/components/OurValues/OurValues.css.js
@@ -8,26 +8,26 @@ const Section = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	padding-top: 60px;
+	padding-top: 3.75rem;
 `;
 
 const Headline1 = styled.header`
 	font-family: 'Rubik', sans-serif;
 	color: #255957;
-	font-size: 56px;
+	font-size: 3.5rem;
 	font-weight: bold;
 `;
 const Headline2 = styled.header`
   font-family: 'Rubik', sans-serif;
   font-weight: medium;
-  font-size: 28px;
+  font-size: 1.75rem;
   color: #333333
-  padding-top: 10px;
+  padding-top: 1rem;
 `;
 const ValueSection = styled.div`
 	display: flex;
 	flex-direction: row;
-	padding-top: 60px;
+	padding-top: 5rem;
 
 	@media screen and (max-width: 769px) {
 		flex-direction: column;
@@ -44,12 +44,12 @@ const Body = styled.div`
   font-family: 'Karla', sans-serif;
   font-size: 16px;
   font-weight: regular;
-  line-height: 30px;
+  line-height: 2rem;
   color: #333333
   text-align: center;
-  padding: 19px 20px 86px 20px;`;
+  padding: 1rem 2rem 5rem 2rem;`;
 
 const IconImg = styled.img`
-  height: 116px;`;
+  height: 8rem;`;
 
 export { Section, Headline1, Headline2, ValueSection, Value, Body, IconImg };
