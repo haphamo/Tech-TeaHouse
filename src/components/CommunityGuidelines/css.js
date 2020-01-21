312ad0475e6a53b16bbceea88e376ae4794ec7a3
jenbeltran <francescajenbeltran@gmail.com>
2020-01-20 21:35:25 -0500

    added community guidlines

--- src/components/CommunityGuidelines/css.js
+++ src/components/CommunityGuidelines/css.js
@@ -1,12 +1,12 @@
-import { H2, H3 } from "../../theme/typography"
-import styled from "styled-components"
+import { H2, H3 } from '../../theme/typography';
+import styled from 'styled-components';
 
 export const CommunityH2 = styled(H2)`
   text-align: center;
   margin-top: 0;
-`
+`;
 
 export const CommunityH3 = styled(H3)`
   text-align: center;
   margin-top: 0;
-`
+`;
