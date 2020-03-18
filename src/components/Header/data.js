20f715763924422b6cd281c04ebf427afdb9c7e5
haphamo <hapham0392@gmail.com>
2020-03-18 16:09:01 -0400

    Updated the data in the header, get in touch has been changed to join us

--- src/components/Header/data.js
+++ src/components/Header/data.js
@@ -40,8 +40,8 @@ const data = [
     image: womenInTechSeven,
   },
   {
-    pageName: "get-in-touch",
-    headerOneContent: "Get in Touch",
+    pageName: "join-us",
+    headerOneContent: "Join Us",
     paragraphContent:
       "Thank you for your interest in supporting #TechTeahouse! We’re working together to build an inclusive Toronto tech community. Come join the conversation under our hashtag #TechTeahouse to stay up to date on new events and meetups near you. Let’s brew more diversi-tea together!",
   },
