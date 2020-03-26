c7309b1163ed8f7f0206511f6bf768671eb532f5
haphamo <hapham0392@gmail.com>
2020-03-25 21:37:05 -0400

    no more plugin error

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,11 @@ module.exports = {
 				display          : `minimal-ui`,
 				icon             : `src/images/favicon.png` // This path is relative to the root of the site.
 			}
-			},
-			{
-				resolve: `gatsby-plugin-styled-components`,
-			}
+		},
+		{
+			resolve: `gatsby-plugin-styled-components`,
 		}
+		
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
